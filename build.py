708e58e2b51a571540b0f5c41377612b4406020e
Celso Providelo <celso.providelo@gmail.com>
2018-03-01 03:54:34 +0000

    Feching *complete* current.json directly from snapfind.
    Using the new GET /sections/snaps endpoint this script is not restricted to a specific architecture or the first 20 snaps assigned to a section.
    In the near future, the required APIs will be exposed via dashboard.s.io.

--- build.py
+++ build.py
@@ -9,32 +9,11 @@ import logging
 import requests
 
 
-logging.basicConfig(format='%(asctime)s %(levelname)-5.5s %(message)s')
+logging.basicConfig(format='%(asctime)s %(levelname)-4.4s  %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 name_cache = {}
-
-
-# Both API calls only consider 'amd64' for now ...
-
-
-def get_current_sections():
-    r = requests.get('https://api.snapcraft.io/api/v1/snaps/sections')
-    section_names = [
-        s['name'] for s in r.json()['_embedded']['clickindex:sections']]
-    sections = {}
-    for name in section_names:
-        r = requests.get(
-            'https://api.snapcraft.io/api/v1/snaps/search'
-            '?confinement=classic,strict&section={}&fields=snap_id'
-            .format(name))
-        snap_ids = [
-            s['snap_id'] for s in r.json()['_embedded']['clickindex:package']]
-        logger.info('+++ Fetched {} ({} entries)'.format(name, len(snap_ids)))
-        sections[name] = snap_ids
-
-    return sections
 
 
 def get_snap_id(name):
@@ -58,47 +37,17 @@ def main():
     parser = argparse.ArgumentParser(
         description='Section Operations ...'
     )
-    parser.add_argument('-c', '--current', metavar='CURRENT.JSON',
-                        help='Patch to existing current.json')
     args = parser.parse_args()
 
-    if args.current:
-        logger.info('Trying to load current section ...')
-        try:
-            with open(args.current) as fd:
-                payload = json.load(fd)
-                current_sections = {
-                    item['section_name']: [s['snap_id'] for s in item['snaps']]
-                    for item in payload['sections']}
-        except FileNotFoundError:
-            logger.error('Could not open {!r}'.format(args.current))
-            return
-        except json.decoder.JSONDecodeError:
-            logger.error('Could not parse {!r}'.format(args.current))
-            return
-    else:
-        logger.info('Fetching current sections ...')
-        current_sections = get_current_sections()
-        current_payload = {'sections': []}
-        for section_name in sorted(current_sections.keys()):
-            snap_ids = current_sections[section_name]
-            snaps = []
-            for i, snap_id in enumerate(snap_ids):
-                snap = {
-                    'series': '16',
-                    'snap_id': snap_id,
-                    'featured': i < 20,
-                    'score': len(snap_ids) - i,
-                }
-                snaps.append(snap)
-            current_payload['sections'].append({
-                'section_name': section_name,
-                'snaps': snaps,
-            })
-
-        logger.info('Saving "current.json" ¯\_(ツ)_/¯ ...')
-        with open('current.json', 'w') as fd:
-            fd.write(json.dumps(current_payload, indent=2, sort_keys=True))
+    current_sections = {}
+    if not os.path.exists('current.json'):
+        logger.warning('Could not find "current.json" ¯\_(ツ)_/¯ ...')
+        logger.warning('Generate it from the snapfind instance:')
+        logger.warning('')
+        logger.warning("  $ curl http://localhost:8003/sections/snaps | "
+                       "jq '.' > /tmp/current.json")
+        logger.warning('')
+        logger.warning('Otherwise deletions cannot be calculated.')
 
     logger.info('Processing new sections ...')
     new_sections = {}
@@ -123,29 +72,6 @@ def main():
             snap_ids.append(snap_id)
         new_sections[section_name] = snap_ids
 
-    # Assembly deletion payload.
-    logger.info('Calculating snap deletions ...')
-    delete_sections = []
-    delete_payload = {'sections': []}
-    for section_name in sorted(current_sections.keys()):
-        if section_name not in new_sections.keys():
-            delete_sections.append(section_name)
-            continue
-        snap_ids = list(
-            set(current_sections[section_name]) -
-            set(new_sections[section_name]))
-        if not snap_ids:
-            continue
-        delete_payload['sections'].append({
-            'section_name': section_name,
-            'snaps': [
-                {'series': '16', 'snap_id': s} for s in sorted(snap_ids)],
-        })
-
-    logger.info('Saving "delete.json" ...')
-    with open('delete.json', 'w') as fd:
-        fd.write(json.dumps(delete_payload, indent=2, sort_keys=True))
-
     logger.info('Calculating snap updates ...')
     update_payload = {'sections': []}
     for section_name in sorted(new_sections.keys()):
@@ -168,12 +94,39 @@ def main():
     with open('update.json', 'w') as fd:
         fd.write(json.dumps(update_payload, indent=2, sort_keys=True))
 
+    # Assembly deletion payload.
+    logger.info('Calculating snap deletions ...')
+    delete_sections = []
+    delete_payload = {'sections': []}
+    for section_name in sorted(current_sections.keys()):
+        if section_name not in new_sections.keys():
+            delete_sections.append(section_name)
+            continue
+        snap_ids = list(
+            set(current_sections[section_name]) -
+            set(new_sections[section_name]))
+        if not snap_ids:
+            continue
+        delete_payload['sections'].append({
+            'section_name': section_name,
+            'snaps': [
+                {'series': '16', 'snap_id': s} for s in sorted(snap_ids)],
+        })
+
+    if delete_payload['sections']:
+        logger.info('Saving "delete.json" ...')
+        with open('delete.json', 'w') as fd:
+            fd.write(json.dumps(delete_payload, indent=2, sort_keys=True))
+    else:
+        logger.info('No deletions needed.')
+
     print(72 * '=')
     print('Copy "delete.json" and "update.json" to a snapfind instance. '
           'Then run the following commands:')
     print()
-    print("  $ curl -X DELETE -H 'Content-Type: application/json' "
-          "http://localhost:8003/sections/snaps -d '@delete.json'")
+    if delete_payload['sections']:
+        print("  $ curl -X DELETE -H 'Content-Type: application/json' "
+              "http://localhost:8003/sections/snaps -d '@delete.json'")
     print("  $ curl -X POST -H 'Content-Type: application/json' "
           "http://localhost:8003/sections/snaps -d '@update.json'")
     if delete_sections:
